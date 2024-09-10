02afea24fc4a370b0fb77a5e04350c9ec1ccbd21
Adão Lima <adaow1257@gmail.com>
2024-09-10 15:20:31 -0300

    rota para add os goals

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,38 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createGoal } from '../functions/create-goal'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
